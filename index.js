606ca8b497f6aaa29eff11e20f6fc6fb6189d72b
Johnny Rocket <0.snilcy@gmail.com>
2020-12-30 02:14:52 +0300

    update

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core")
+const github = require("@actions/github")
+const fs = require("fs")
+const path = require("path")
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  const repoName = github.event.repository.name
+  console.log(`Repository name ${repoName}!`)
+
+  const routerFilePath = path.join(__dirname, core.getInput("router-file"))
+  console.log(`Router file`, routerFilePath)
+
+  const routerFile = fs.readFileSync(routerFilePath, "utf-8")
+  fs.writeFileSync(
+    routerFilePath,
+    routerFile.replace(
+      "<BrowserRouter>",
+      `<BrowserRouter basename="/${repoName}">`
+    )
+  )
+
+  const time = new Date().toTimeString()
+  core.setOutput("time", time)
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  console.log(`The event payload: ${payload}`)
 } catch (error) {
-  core.setFailed(error.message);
+  core.setFailed(error.message)
 }
