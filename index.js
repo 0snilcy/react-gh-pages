38c774c2b4304914cd8df51b2aa0650ddab64d04
Johnny Rocket <0.snilcy@gmail.com>
2020-12-30 02:18:02 +0300

    update

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require("fs")
 const path = require("path")
 
 try {
+  console.log(github, github.context)
   const repoName = github.event.repository.name
   console.log(`Repository name ${repoName}!`)
 
